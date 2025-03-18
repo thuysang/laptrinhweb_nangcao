b50d12797f3c267391a3b09af24637c4b10d8be5
Thsann <132922816+thuysang@users.noreply.github.com>
2025-03-18 08:20:24 +0700

    Add files via upload

---
+++ Form/Bai1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtHoTen = new TextBox();
+            btnHienThi = new Button();
+            btnThoat = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlLight;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(160, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Họ Và Tên";
+            // 
+            // txtHoTen
+            // 
+            txtHoTen.Location = new Point(278, 124);
+            txtHoTen.Name = "txtHoTen";
+            txtHoTen.Size = new Size(329, 27);
+            txtHoTen.TabIndex = 1;
+            // 
+            // btnHienThi
+            // 
+            btnHienThi.BackColor = Color.FromArgb(255, 192, 192);
+            btnHienThi.Location = new Point(226, 238);
+            btnHienThi.Name = "btnHienThi";
+            btnHienThi.Size = new Size(94, 29);
+            btnHienThi.TabIndex = 2;
+            btnHienThi.Text = "Hiển Thị";
+            btnHienThi.UseVisualStyleBackColor = false;
+            btnHienThi.Click += btnHienThi_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.BackColor = Color.SkyBlue;
+            btnThoat.Location = new Point(439, 238);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(94, 29);
+            btnThoat.TabIndex = 3;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = false;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnThoat);
+            Controls.Add(btnHienThi);
+            Controls.Add(txtHoTen);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            MouseClick += Form1_MouseClick;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtHoTen;
+        private Button btnHienThi;
+        private Button btnThoat;
+    }
+}
---
+++ Form/Bai1/Form1.cs
@@ -0,0 +1,58 @@
+﻿namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHienThi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tên của bạn là: " + txtHoTen.Text);
+        }
+
+
+
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Bạn vừa click chuột phải");
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show("Bạn vừa click chuột trái");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn mở ứng dụng?", "Hỏi người dùng",
+               MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                Dispose(); // Đóng ứng dụng nếu chọn Cancel
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyCode == Keys.H)
+            {
+                txtHoTen.Text = "Hello Freetuts.net";
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát không?", "Xác nhận thoát",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit(); // Thoát chương trình
+            }
+    }
+}
+}

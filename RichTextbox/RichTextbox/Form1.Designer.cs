2ef3785380b23e3623bb3b3ccc9188c8dd1dd9ee
Thsann <132922816+thuysang@users.noreply.github.com>
2025-03-18 08:26:16 +0700

    Add files via upload

---
+++ RichTextbox/RichTextbox/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RichTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = Color.LightSeaGreen;
+            richTextBox1.Location = new Point(0, 0);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(800, 450);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "RichTexBox controls allow you to display or edit flow content, including paragraphs, images, tables, etc.\n\n";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ RichTextbox/RichTextbox/Form1.cs
@@ -0,0 +1,25 @@
+﻿namespace RichTextbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            CreaRichTextBox();
+        }
+        public void CreaRichTextBox()
+        {
+            RichTextBox rtbData = new RichTextBox();
+            rtbData.Dock = DockStyle.Fill;
+            rtbData.Find("Richtexbox", RichTextBoxFinds.MatchCase);
+            rtbData.SelectionFont = new Font("Verdata", 12, FontStyle.Bold);
+            rtbData.SelectionColor = Color.Pink;
+            rtbData.SaveFile("MyData.rft", RichTextBoxStreamType.RichText);
+            this.Controls.Add(rtbData);
+        }
+    }
+}
